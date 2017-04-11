2177c3cb013c8407d54334e0319a8c4fcc5dc4f5
cindyk09 <cindyk09@gmail.com>
2017-04-11 16:00:45 -0400

    updated private params for devise in registration controller

--- wardesportsapp/app/controllers/registrations_controller.rb
+++ wardesportsapp/app/controllers/registrations_controller.rb
@@ -4,8 +4,12 @@ class RegistrationsController <  Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :address1, :address2, :city, :state, :zipcode, :phone_number) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
+      :firstname, :lastname, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+      :username, :firstname, :lastname, :description, :email, :password, :password_confirmation,
+      :address1, :address2, :city, :state, :country, :postalcode, :admin,
+      :organizer, :email) }
   end
   def update_resource(resource, params)
    resource.update_without_password(params)
