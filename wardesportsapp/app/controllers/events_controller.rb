6a33b554f8d17fa85a37f50320ecaae88697312b
cindyk09 <cindyk09@gmail.com>
2017-04-20 14:51:37 -0400

    forgot to add before_authenticate to events controller

--- wardesportsapp/app/controllers/events_controller.rb
+++ wardesportsapp/app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   # before_action :set_event (:update, :edit, :destroy)
 
   def index
